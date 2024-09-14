9309d095a11c3fff1987314ec40ddcdbb7c66f56
fuki_fuki <fuuki1228@icloud.com>
2024-09-15 02:08:02 +0900

    refactor: Remove duplicate import statement in home page component

--- app/src/app/home/page.tsx
+++ app/src/app/home/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 const Home: React.FC = () => {
   const router = useRouter();
